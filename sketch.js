3a305e9d20387e9d3e8b150ea80028b861c7ade5
hridayabala <78938705+hridayabala@users.noreply.github.com>
2021-03-11 19:02:15 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,19 +16,20 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 	
-	paperObject = new paper(200,450,70);
-	groundObject = new ground(width/2,670,width,20);
-	dustbinObj = new dustbin(1200,650);	
+	paperObject=new paper(200,450,70);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	//Create a Ground
+	
 
 	var render = Render.create({
-	  element : document.body,
-	  engine : engine,
-	  options : {
-	    width : 1600,
-	    height : 700,
-	    wireframes : false
-	
-	}
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1600,
+	    height: 700,
+	    wireframes: false
+	  }
 	});
 
 	Engine.run(engine);
@@ -36,20 +37,34 @@ function setup() {
   
 }
 
+
 function draw() {
   rectMode(CENTER);
   background(230);
+ 
   
   paperObject.display();
   groundObject.display();
   dustbinObj.display();
 
+  
+  
+ 
+  
+  
+ 
 }
 
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
+
     	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
 
+    
   	}
-
 }
+
+
+
+
+
